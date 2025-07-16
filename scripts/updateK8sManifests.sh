a09ee27d76dc567d3f104bf929b9f2f502e24adf
e.shahid2002 <e.shahid2002@gmail.com>
2025-07-16 07:04:53 +0000

    Updated updateK8sManifests.sh

--- scripts/updateK8sManifests.sh
+++ scripts/updateK8sManifests.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -xe
 
+DEPLOYMENT=$1
+IMAGENAME=$2
+TAG=$3
 PAT=$4
 
 REPO_URL="https://$PAT@dev.azure.com/Practice-Proj/voting-app/_git/voting-app"
@@ -9,11 +12,10 @@ git clone "$REPO_URL" /tmp/temp_repo
 
 cd /tmp/temp_repo
 
-# Update image tag
-sed -i "s|image:.*|image: emanshahidazurecicd/$2:$3|g" k8s-specifications/$1-deployment.yaml
+sed -i "s|image:.*|image: emanshahidazurecicd/$IMAGENAME:$TAG|g" k8s-specifications/${DEPLOYMENT}-deployment.yaml
 
 git add .
-git commit -m "Update kubernetes manifest with new image tag"
+git commit -m "Update image tag to $IMAGENAME:$TAG"
 git push
 
 rm -rf /tmp/temp_repo
