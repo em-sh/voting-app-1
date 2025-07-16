71008c064b2887ae9cf96db7e6701a5ce8b0ea70
e.shahid2002 <e.shahid2002@gmail.com>
2025-07-16 06:15:26 +0000

    Added updateK8sManifests.sh

---
+++ scripts/updateK8sManifests.sh
@@ -0,0 +1,30 @@
+#/bin/bash
+
+set -x
+
+# Set the repository URL
+REPO_URL="https://Fx3iyZ7HKfirv00MfjRKDncSMPOj1jbVipkegS5WOr99bzAOBqtCJQQJ99BGACAAAAAAAAAAAAASAZDO2JZX@dev.azure.com/Practice-Proj/voting-app/_git/voting-app"
+
+# clone the repo
+git clone "REPO_URL" /tmp/temp_repo
+
+# navigate to the cloned repo
+cd /tmp/temp_repo
+
+# make changes to the image tag in a deployment.yaml file
+sed -i "s|image:.*|image: emanshahidazurecicd/$2:$3|g" k8s-specifications/$1-deployment.yaml
+
+# add the changes
+
+git add .
+
+# commit the changes
+git commit -m "Update kubernetes manifest"
+
+# push the changes to repo
+git push
+
+# clean
+
+rm -rf /tmp/temp_repo
+
